c8f8d61e6a29607f6d66920749fd9f1ba3ed01ba
Maria Ivanilova <ivanilovamaria@gmail.com>
2025-06-06 18:41:44 +0300

    очередной безуспешный видимо тест

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
-
 set -e
 
 host="$1"
 port="$2"
 shift 2
-cmd="$*"
+cmd="$@"
 
-# Wait for the port to become available
+# Ожидание доступности порта
 until nc -z "$host" "$port"; do
   >&2 echo "Waiting for $host:$port to become available..."
   sleep 1
 done
 
 >&2 echo "$host:$port is available, executing command: $cmd"
-# shellcheck disable=SC2086
-# exec $cmd
-# exec "$@"
-sh -c "$@"
+exec $cmd
